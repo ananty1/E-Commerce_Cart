49d49fb24118f2abbf6510d073f32a88e9bf0a20
anant <ananty@iitbhilai.ac.in>
2024-08-18 23:20:57 +0530

    Added Product header section of the product listing page

--- src/app/components/ProductHeader.js
+++ src/app/components/ProductHeader.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/navigation';
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaStore } from "react-icons/fa";
 
 export default function ProductsHeader({ cart }) {
   const router = useRouter();
@@ -9,14 +9,20 @@ export default function ProductsHeader({ cart }) {
   };
 
   return (
-    <div className="sticky top-0 z-50 flex flex-row justify-between items-center">
-      <h1 className="text-3xl font-bold mb-8">Products</h1>
+    <div className="sticky top-0 z-50 flex flex-row justify-around items-center">
+      <div className="flex items-center">
+        <FaStore className="text-3xl text-blue-500 mr-2" />
+        <h1 className="text-3xl font-bold mb-0">Products</h1>
+      </div>
       <button
         className="text-xl font-bold mb-8 bg-blue-500 text-white py-2 px-4 rounded"
         onClick={handleNavigateToCart}
       >
+        <div className="flex items-center">
+        <FaShoppingCart className="text-xl  mr-2" />
+        <h1 className="text-xl font-bold mb-0">Cart ({cart.length})</h1>
+      </div>
         
-        <span><FaShoppingCart/>  View Cart ({cart.length})</span>
       </button>
     </div>
   );
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -11,7 +11,7 @@ import ProductsHeader from '../components/ProductHeader';
 export default function Home() {
   const { cart,addToCart } = useCart(); // Use the context hook to get addToCart function
   const [products, setProducts] = useState([]);
-  const itemsToFetch = 10; // Number of items you want to fetch
+  const itemsToFetch = 20; // Number of items you want to fetch
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <ProductsHeader cart={cart} />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map(product => (
           <ProductCard 
             key={product.id} 
